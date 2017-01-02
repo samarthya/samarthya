4d78fa0063edd4512a8baafc0f435ff7f9464d47
Saurabh Sharma <saurabh.sharma@pb.com>
2017-01-02 17:30:08 +0530

    Component introduced

--- resources/app/app.js
+++ resources/app/app.js
@@ -8,27 +8,8 @@
  * The first module of the application.
  * @type {*|angular.Module}
  */
-var app = angular.module("personalApp",[]);
+var app = angular.module('personalApp',['ancillary']);
 
-/**
- * @returns {$q} Returns a promise that makes a call to the MongoDB server in the back
- * returning all the feedback that has been recieved in the page.
- */
-app.factory('commentsSrvc', ['$log', '$http', '$q', function($log, $http, $q){
-        return $q(function(resolve, reject){
-            $http({
-                method: 'GET',
-                url: '/comments'
-            }).then(function successCallback(response) {
-                $log.log(response);
-                resolve(response.data);
-            }, function errorCallback(response) {
-                $log.log('error');
-                reject(' Failed');
-            });
-        });
-
-    }]);
 
 /**
  * @description The main controller for the module:personalApp. It populates the initial comments
@@ -37,15 +18,4 @@ app.factory('commentsSrvc', ['$log', '$http', '$q', function($log, $http, $q){
 app.controller('mainController', ['$scope', 'commentsSrvc', function($scope, commentsSrvc){
     $scope.welcomeMessage = "This application will use Angular";
 
-
-    $scope._getComments = function () {
-        console.log(' I got comments');
-        commentsSrvc.then(function (commentsFromDB) {
-            $scope.comments = commentsFromDB;
-        }, function (reason) {
-            console.log(' It failed');
-        });
-    };
-
-    $scope.comments = $scope._getComments();
 }]);
---
+++ resources/app/components/comments/comments.component.js
@@ -0,0 +1,82 @@
+/**
+ * Created by saurabhs on 02/01/17.
+ */
+
+/**
+ * Ancillary module that would contain some functions.
+ * @type {*|angular.Module}
+ */
+var ancillary = angular.module('ancillary', []);
+
+/**
+ * @returns {$q} Returns a promise that makes a call to the MongoDB server in the back
+ * returning all the feedback that has been recieved in the page.
+ */
+ancillary.factory('commentsSrvc', ['$log', '$http', '$q', function ($log, $http, $q) {
+
+    return $q(function (resolve, reject) {
+        $http({
+            method: 'GET',
+            url: '/comments'
+        }).then(function successCallback(response, status, header, config) {
+            $log.log(status + ':' + response);
+            resolve(response);
+        }, function errorCallback(response) {
+            $log.log('error');
+            reject(' Failed');
+        });
+    });
+
+}]);
+
+
+
+
+ancillary.component('allComments', {
+    /**
+     * Template for the comments.
+     */
+    templateUrl: 'app/components/comments/comments.html',
+
+    /**
+     * Controller for the Comments.
+     */
+    controller: ['$log', '$q', 'commentsSrvc',
+        function commentsController($log, $q, commentsSrvc ) {
+
+            var self = this;
+
+            /**
+             * Method to make mongoDB call to fetch the comments via Service.
+             * @private
+             */
+            this._getComments = function () {
+                if (commentsSrvc != undefined) {
+                    $log.log(' I got comments');
+                    commentsSrvc.then(function success (commentsFromDB) {
+                            self.objComments = commentsFromDB.data;
+                            $log.log(commentsFromDB.status + '::' + commentsFromDB.data);
+                        },
+                        function failure (reason) {
+                            $log.log(' Fetch comments - Failed!');
+                        });
+                } else {
+                    $log.log(' Cannot load - the service');
+                }
+            };
+
+            /**
+             * Comments variable.
+             */
+            this.objComments = this._getComments();
+
+            this.$onInit = function () {
+                $log.log('On init called');
+            }
+        }]
+
+});
+
+ancillary.run(function ($log) {
+    $log.info(' Inside run');
+});
