71406108028249a70d525c5771d3cad9fdb226bf
Saurabh Sharma <saurabh.sharma@pb.com>
2017-01-05 16:48:32 +0530

    Added  about me

--- resources/app/components/aboutme/aboutme.component.js
+++ resources/app/components/aboutme/aboutme.component.js
@@ -1,3 +1,12 @@
 /**
  * Created by saurabhs on 02/01/17.
  */
+
+angular.module('ancillary').component('aboutMe', {
+    templateUrl: 'app/components/aboutme/aboutme.html',
+
+    controller: ['$log', function myWelcomeMessage($log) {
+        $log.log('Welcome called');
+
+    }]
+});
